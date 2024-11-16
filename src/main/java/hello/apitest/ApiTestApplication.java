9ddac48f270df4cb4293ce52ca4047cb4afaf291
rlaqudwn1 <rlaqudwn123456@gmail.com>
2024-11-17 02:27:55 +0900

    아 제발

--- src/main/java/hello/apitest/ApiTestApplication.java
+++ src/main/java/hello/apitest/ApiTestApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ApiTestApplication {
-
+//
     public static void main(String[] args) {
         SpringApplication.run(ApiTestApplication.class, args);
     }
